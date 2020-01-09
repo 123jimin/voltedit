abd0c3c9f800822eaafe133c4a0cc75f3dcf0a74
Jimin Park <pjm950@gmail.com>
2020-01-09 13:26:12 +0900

    Added an implementation of AA tree
    To easily manage placed objects

--- js/util.js
+++ js/util.js
@@ -11,3 +11,247 @@ const CLIP = (x, a, b) => x<a ? a : x>b ? b : !isFinite(x) ? a : x;
 
 /// Align to the value
 const ALIGN = (a, x) => x%a === 0 ? x : RD(x/a)*a;
+
+/// AA tree for managing non-overlapping intervals
+class AATree {
+	constructor() {
+		this.size = 0;
+		this.root = null;
+	}
+	add(y, l, data) {
+		const node = new AATreeNode(y, l, data);
+		if(this.root) {
+			const prev = this.root._insert(node);
+			if(prev) {
+				return [false, prev];
+			} else {
+				++this.size;
+				return [true, node];
+			}
+		} else {
+			this._setChild(node);
+			++this.size;
+			return [true, node];
+		}
+	}
+	get(y) {
+		return this.root ? this.root._get(y) : null;
+	}
+	_setChild(node) {
+		this.root = node;
+		node._parent = this;
+	}
+	_removeChild(node) {
+		this.root = null;
+	}
+	_check() {
+		return this.root ? this.root._check() : null;
+	}
+	_dump() {
+		return this.root ? this.root._dump('') : "";
+	}
+	_adjustAfterRemove() {}
+	_notifyRemove() {
+		--this.size;
+	}
+}
+class AATreeNode {
+	constructor(y, l, data) {
+		this.y = y;
+		this.l = l;
+		this.data = data;
+
+		this._level = 1;
+		this._left = null;
+		this._right = null;
+		this._parent = null;
+	}
+	remove() {
+		this._notifyRemove();
+		this._remove(null);
+	}
+	next() {
+		if(!this._right) return null;
+		let n = this._right;
+		while(n._left) n = n._left;
+		return n;
+	}
+	prev() {
+		if(!this._left) return null;
+		let p = this._left;
+		while(p._right) p = p._right;
+		return p;
+	}
+	_remove(from) {
+		if(this._isLeaf()){
+			const parent = this._parent;
+			parent._removeChild(this);
+			parent._adjustAfterRemove(from);
+			return;
+		}else if(!this._left){
+			const l = this.next();
+			l._remove(this);
+			this._set(l);
+		}else{
+			const l = this.prev();
+			l._remove(this);
+			this._set(l);
+		}
+		this._adjustAfterRemove(from);
+	}
+	_adjustAfterRemove(stop) {
+		if(this === stop) return;
+		const parent = this._parent;
+		this._decreaseLevel();
+		let newTree = this._skew();
+		if(newTree._right){
+			newTree._right._skew();
+		}
+		if(newTree._right && newTree._right._right){
+			newTree._right._right._skew();
+		}
+		newTree = newTree._split();
+		if(newTree._right){
+			newTree._right._split();
+		}
+		parent._adjustAfterRemove(stop);
+	}
+	_set(node) {
+		this.y = node.y;
+		this.l = node.l;
+		this.data = node.data;
+	}
+	_notifyRemove() {
+		this._parent._notifyRemove();
+	}
+	_isLeaf() {
+		return !(this._left || this._right);
+	}
+	_get(y) {
+		if(y < this.y) {
+			return this._left ? this._left._get(y) : null;
+		} else if(y > this.y+this.l) {
+			return this._right ? this._right._get(y) : null;
+		}
+		return this;
+	}
+	_replaceChild(node, repl) {
+		if(node.y < this.y) this._left = repl;
+		else this._right = repl;
+
+		if(repl) repl._parent = this;
+	}
+	_setChild(node) {
+		this._replaceChild(node, node);
+	}
+	_removeChild(node) {
+		this._replaceChild(node, null);
+	}
+	_insert(node) {
+		if(node.y === this.y) {
+			return this;
+		} else if(node.y <= this.y) {
+			if(node.y + node.l >= this.y) {
+				return this;
+			}
+			if(this._left) {
+				const result = this._left._insert(node);
+				if(result) return result;
+			} else {
+				this._setChild(node);
+			}
+		} else if(node.y > this.y) {
+			if(this.y + this.l >= node.y) {
+				return this;
+			}
+			if(this._right) {
+				const result = this._right._insert(node);
+				if(result) return result;
+			} else {
+				this._setChild(node);
+			}
+		}
+		this._skew()._split();
+		return null;
+	}
+	_skew() {
+		if(!this._left) return this;
+		if(this._left._level !== this._level) return this;
+		const left = this._left;
+		const parent = this._parent;
+		if(left._right) {
+			this._setChild(left._right);
+		} else {
+			this._left = null;
+		}
+		left._setChild(this);
+		parent._setChild(left);
+		return left;
+	}
+	_split() {
+		if(!this._right || !this._right._right) return this;
+		if(this._level !== this._right._right._level) return this;
+		const right = this._right;
+		const parent = this._parent;
+		if(right._left) {
+			this._setChild(right._left);
+		} else {
+			this._right = null;
+		}
+		right._setChild(this);
+		parent._setChild(right);
+		++right._level;
+		return right;
+	}
+	_decreaseLevel() {
+		const levelOf = (node) => node ? node._level : 0;
+		const shouldBe = Math.min(levelOf(this._left), levelOf(this._right))+1;
+		if(shouldBe < this._level){
+			this._level = shouldBe;
+			if(shouldBe < levelOf(this._right)){
+				this._right._level = shouldBe;
+			}
+		}
+	}
+	_check() {
+		if(this._isLeaf()){
+			return this._level === 1 ? null : [this, "leaf level"];
+		}
+		if(this._left){
+			const ch = this._left._check();
+			if(ch) return ch;
+			if(this._left.y + this._left.l >= this.y){
+				return [this, "left value"];
+			}
+			if(this._left._level !== this._level-1){
+				return [this, "left level"];
+			}
+		}else if(this._level !== 1){
+			return [this, "null left level"];
+		}
+		if(this._right){
+			const ch = this._right._check();
+			if(ch) return ch;
+			if(this.y + this.l >= this._right.y){
+				return [this, "right value"];
+			}
+			if(this._right._level > this._level || this._right._level < this._level-1){
+				return [this, "right level"];
+			}
+			if(this._right._right && this._right._right._level >= this._level){
+				return [this, "right right level"];
+			}
+		}
+		return null;
+	}
+	_dump(indent) {
+		const oneIndent = '    ';
+		let str = `${indent}: ${this._level} ${this.y} ${this.l}\n`;
+		if(this._isLeaf()) return str;
+		if(this._left) str += this._left._dump(indent+oneIndent);
+		else str += `${indent}${oneIndent}: left null\n`
+		if(this._right) str += this._right._dump(indent+oneIndent);
+		else str += `${indent}${oneIndent}: right null\n`
+		return str;
+	}
+}
