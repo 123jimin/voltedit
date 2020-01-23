b98b3aba6a6e6899908f669721b299c19e751b4d
Jimin Park <pjm950@gmail.com>
2020-01-23 16:23:31 +0900

    Added constructors for each VGraphPointTask

--- js/edit/task.js
+++ js/edit/task.js
@@ -6,7 +6,7 @@ class VEmptyTask extends VTask {
 	_makeInverse() { return this; }
 }
 
-/// A task that mayb be failed silently
+/// A task that may be failed silently
 class VMaybeTask extends VTask {
 	constructor(task) {
 		super(task.editor);
@@ -125,4 +125,61 @@ class VNoteDelTask extends VTask {
 	}
 }
 
-/// TODO: create a list of graph-related tasks
+class VGraphPointTask extends VTask {
+	constructor(editor, points, tick) {
+		super(editor);
+		this.points = points;
+		this.tick = tick;
+	}
+}
+
+/// Create a new point, with optional flags for connectedness of the previous point
+class VGraphPointAddTask extends VGraphPointTask {
+	constructor(editor, points, tick, newPoint, connectPrev) {
+		super(editor, points, tick);
+		this.newPoint = newPoint;
+		this.connectPrev = connectPrev;
+	}
+}
+
+/// Remove the current point, with an optional flag for connectedness of the previous point
+class VGraphPointDelTask extends VGraphPointTask {
+	constructor(editor, points, tick, connectPrev) {
+		super(editor, points, tick);
+		this.connectPrev = connectPrev;
+	}
+}
+
+/// Change v and vf of a point
+class VGraphPointChangeSlamTask extends VGraphPointTask {
+	constructor(editor, points, tick, v, vf) {
+		super(editor, points, tick);
+		this.v = v;
+		this.vf = vf;
+	}
+}
+
+/// Change wide of a point
+class VGraphPointChangeWideTask extends VGraphPointTask {
+	constructor(editor, points, tick, wide) {
+		super(editor, points, tick);
+		this.wide = wide;
+	}
+}
+
+/// Change `connected` value of a point (connect/disconnect an edge)
+class VGraphPointChangeConnectedTask extends VGraphPointTask {
+	constructor(editor, points, tick, connected) {
+		super(editor, points, tick);
+		this.connected = connected;
+	}
+}
+
+/// Change a and b of a point (implement it later)
+class VGraphPointChangeSlamTask extends VGraphPointTask {
+	constructor(editor, points, tick, a, b) {
+		super(editor, points, tick);
+		this.a = a;
+		this.b = b;
+	}
+}
